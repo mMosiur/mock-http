283dc458d0329d81747687dbdf73aaa95da45489
Mateusz Piotr Moruś <mpmorus@gmail.com>
2023-06-23 11:47:35 +0200

    functions

--- Functions/LogRequest.cs
+++ Functions/LogRequest.cs
@@ -6,18 +6,38 @@ namespace MockHttp.Functions;
 
 public static class LogRequest
 {
-    [Function("LogRequest")]
+    [Function(nameof(LogRequest))]
     public static async Task<HttpResponseData> Run(
         [HttpTrigger(AuthorizationLevel.Function, "GET", "PUT", "POST", Route = "log-request/{*path}")]
         HttpRequestData request,
         string? path,
         FunctionContext executionContext)
     {
+        const string contentFilename = "./content.txt";
+
+        var content = File.Exists(contentFilename) ? await File.ReadAllTextAsync(contentFilename) : string.Empty;
+
         var response = request.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        await response.WriteStringAsync($"Path was '{path ?? "null"}'");
-
+        await response.WriteStringAsync($"Path was '{path ?? "null"}' and content was '{content}'");
         return response;
+    }
+}
+
+public static class SetContent
+{
+    [Function(nameof(SetContent))]
+    public static async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "POST", Route = "set-content")] HttpRequestData request,
+        FunctionContext executionContext)
+    {
+        const string contentFilename = "./content.txt";
+
+        await using (var fileStream = File.OpenWrite(contentFilename))
+        {
+            await request.Body.CopyToAsync(fileStream);
+        }
+        
+        return request.CreateResponse(HttpStatusCode.NoContent);
     }
 }
